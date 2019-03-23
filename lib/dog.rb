013d5fca25058035c32f55e65aa6499ef6d49215
Chance Smith <chancesmith97@hotmail.com>
2019-03-23 04:12:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,12 @@ class Dog
 
 def initialize(name)
   @name = name 
-  @@all << self  
+  @@all << self.name  
   
 end
 
 def self.all
-  @@all.collect(&:name) {|x| puts x}
+  @@all.collect {|x| puts x}
 end
 
 def self.clear_all
